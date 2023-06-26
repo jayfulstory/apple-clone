71ed51a7f8fdec0de348c6d97599671841d58a08
kim <jayfulkr@gmail.com>
2023-06-26 17:34:07 +0900

    06/26 section3 fin

--- js/main.js
+++ js/main.js
@@ -63,8 +63,15 @@ const sceneInfo = [
       messageC: document.querySelector('#scroll--section__2 .c'),
       pinB: document.querySelector('#scroll--section__2 .b .pin'),
       pinC: document.querySelector('#scroll--section__2 .c .pin'),
+      canvas: document.querySelector('#video--canvas__1'),
+      context: document.querySelector('#video--canvas__1').getContext('2d'),
+      videoImages: [],
     },
     values: {
+      videoImageCount: 960,
+      imageSequence: [0, 959],
+      canvas_opacity__in: [0, 1, { start: 0, end: 0.1 }],
+      canvas_opacity__out: [1, 0, { start: 0.95, end: 1 }],
       messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
       messageB_translateY_in: [30, 0, { start: 0.5, end: 0.55 }],
       messageC_translateY_in: [30, 0, { start: 0.72, end: 0.77 }],
@@ -105,6 +112,12 @@ function setCanvasImages() {
     imgElem.src = `../video/001/IMG_${6726 + i}.JPG`;
     sceneInfo[0].obj.videoImages.push(imgElem);
   }
+  let imgElem2;
+  for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+    imgElem2 = document.createElement('img');
+    imgElem2.src = `../video/002/IMG_${7027 + i}.JPG`;
+    sceneInfo[2].obj.videoImages.push(imgElem2);
+  }
   // console.log(sceneInfo[0].obj.videoImages);
 }
 setCanvasImages();
@@ -131,6 +144,7 @@ function setLayout() {
 
   const heightRatio = window.innerHeight / 1080;
   sceneInfo[0].obj.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+  sceneInfo[2].obj.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
 }
 
 function calcValues(values, currentY) {
@@ -252,6 +266,21 @@ function playAnimation() {
     // case 1:
     //   break;
     case 2:
+      let sequence2 = Math.round(calcValues(values.imageSequence, currentY));
+      obj.context.drawImage(obj.videoImages[sequence2], 0, 0);
+
+      if (scrollRatio <= 0.5) {
+        obj.canvas.style.opacity = calcValues(
+          values.canvas_opacity__in,
+          currentY
+        );
+      } else {
+        obj.canvas.style.opacity = calcValues(
+          values.canvas_opacity__out,
+          currentY
+        );
+      }
+
       if (scrollRatio <= 0.25) {
         obj.messageA.style.opacity = calcValues(
           values.messageA_opacity_in,
